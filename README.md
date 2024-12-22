5c070a4c945c6978e4acc5fce23c5a0c20ed2af8
stubbulon5 <prsrocker@gmail.com>
2024-12-22 11:55:07 +0000

    Updated with example

--- README.md
+++ README.md
@@ -1,13 +1,133 @@
-ofxExternalDrop
-=======
+# ofxExternalDrop
 Cross platform External Drag and Drop functionality for openFrameworks. macOs currently the only functioning implementation. 
 
-I invite the oF community to help put here...
+I invite the oF community to help out here...
 
-Description
------------
+![alt text](ofxaddons_thumbnail.png)
+
+# Description
 An example usage for this is having your app generate a Midi file during DnD operation which you subsequently drop onto a DAW sequencer.
 
-Example usage
-------------
-<TODO>
+# Example usage
+## ofApp.cpp
+```
+#include "ofApp.h"
+#include "ofxExternalDrop.h"
+
+// External drop manager
+ofxExternalDrop externalDrop;
+bool externalDropInitialized = false;
+
+ofxExternalDrop &getExternalDrop()
+{
+    if (!externalDropInitialized)
+    {
+        externalDrop = ofxExternalDrop(true);
+        externalDropInitialized = true;
+    }
+    return externalDrop;
+}
+
+void clearExternalDrop()
+{
+    externalDrop = ofxExternalDrop(false);
+    externalDropInitialized = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofFile newFile(ofToDataPath("temp.txt"), ofFile::WriteOnly);
+    newFile.create(); // now file exists 
+    ofLogNotice("Wrote file `" + ofFilePath::getAbsolutePath(ofToDataPath("temp.txt")) + "` to test ofxExternalDrop - to test - click application area , drag and release out side of application (eg to desktop. File should land there)");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    if (!getExternalDrop().getIsDragging())
+    {
+        // Initiate external Drag and Drop
+        string fileToDrop = ofFilePath::getAbsolutePath(ofToDataPath("temp.txt"));
+        vector<string> dragFiles = vector<string>({fileToDrop});
+        getExternalDrop().externalDragAndDropFileInit(dragFiles);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    if (getExternalDrop().getIsDragging())
+    {
+        getExternalDrop().handleExternalDragAndDropButtonReleaseEvent();
+    }
+    clearExternalDrop();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    if (getExternalDrop().getIsDragging())
+    {
+        getExternalDrop().handleExternalDragAndDropMotionNotify();
+    }
+}
+
+
+```
--- src/platform/osx/externalDragAndDropOSX.h
+++ src/platform/osx/externalDragAndDropOSX.h
@@ -5,89 +5,91 @@
 
 #import "NSDraggingSourceHelper.h"
 
-
-
-class externalDragAndDrop {
+class externalDragAndDrop
+{
 
 public:
-    externalDragAndDrop() {
+    externalDragAndDrop()
+    {
         draggingFinishedSelector = @selector(handleExternalDragAndDropButtonReleaseEvent:);
     }
-    
-    bool getIsDragging() {
+
+    bool getIsDragging()
+    {
         return dragging;
     }
 
-    NSView* getNSViewForDragEvent() {
-        NSWindow* nativeWindow = (__bridge NSWindow*)ofGetCocoaWindow();
+    NSView *getNSViewForDragEvent()
+    {
+        NSWindow *nativeWindow = (__bridge NSWindow *)ofGetCocoaWindow();
         return [nativeWindow contentView];
     }
 
-    bool externalDragAndDropFileInit(vector<string> files) {
-        if (files.size() == 0 || getIsDragging()) return false;
+    bool externalDragAndDropFileInit(vector<string> files)
+    {
+        if (files.size() == 0 || getIsDragging())
+            return false;
         dragging = true;
 
-        NSView* view = getNSViewForDragEvent();
+        NSView *view = getNSViewForDragEvent();
 
-        if (view) {
+        if (view)
+        {
 
-            @autoreleasepool {
+            @autoreleasepool
+            {
 
-                if (auto event = [[view window] currentEvent]) {
-                    auto dragItems = [[NSMutableArray alloc] init];
+                if (auto event = [[view window] currentEvent])
+                {
+                    auto dragItems = [[NSMutableArray alloc] init];
 
-                    for(int i=0; i<files.size(); i++) {
+                    for (int i = 0; i < files.size(); i++)
+                    {
                         cout << "FILE:->" << files[i] << "\n";
                         string filename = files[i];
 
-                        NSString *nsFilename = [NSString stringWithCString:filename.c_str()   
-                            encoding:[NSString defaultCStringEncoding]];  
+                        NSString *nsFilename = [NSString stringWithCString:filename.c_str()
+                                                                  encoding:[NSString defaultCStringEncoding]];
 
-                        auto fileURL = [NSURL fileURLWithPath: nsFilename];
-                        auto dragItem = [[NSDraggingItem alloc] initWithPasteboardWriter: fileURL];
+                        auto fileURL = [NSURL fileURLWithPath:nsFilename];
+                        auto dragItem = [[NSDraggingItem alloc] initWithPasteboardWriter:fileURL];
 
-                        auto eventPos = [event locationInWindow];
-                        auto dragRect = [view convertRect: NSMakeRect (eventPos.x - 16.0f, eventPos.y - 16.0f, 32.0f, 32.0f)
-                                                    fromView: nil];
-                        
-                        auto dragImage = [[NSWorkspace sharedWorkspace] iconForFile: nsFilename];
-                        [dragItem setDraggingFrame: dragRect contents: dragImage];
-                        
-                        NSLog(@"DRAGGED FILE IS %@",nsFilename );
+                        auto eventPos = [event locationInWindow];
+                        auto dragRect = [view convertRect:NSMakeRect(eventPos.x - 16.0f, eventPos.y - 16.0f, 32.0f, 32.0f)
+                                                 fromView:nil];
+                        auto dragImage = [[NSWorkspace sharedWorkspace] iconForFile:nsFilename];
+                        [dragItem setDraggingFrame:dragRect contents:dragImage];
 
-                        [dragItems addObject: dragItem];
-    //                     [dragItem release];
+                        NSLog(@"DRAGGED FILE IS %@", nsFilename);
 
+                        [dragItems addObject:dragItem];
+                        // [dragItem release];
                     }
-                    
-                    auto helper = [[NSDraggingSourceHelper alloc] init];
+
+                    auto helper = [[NSDraggingSourceHelper alloc] init];
 
                     [helper setSource:view andDraggingFinishedSelector:draggingFinishedSelector];
 
-
-                    return [view beginDraggingSessionWithItems: dragItems
-                                                            event: event
-                                                        source: helper] != nullptr;
-
-
-
-                }
+                    return [view beginDraggingSessionWithItems:dragItems
+                                                         event:event
+                                                        source:helper] != nullptr;
+                }
             }
-        }
+        }
 
-        return false;
-    }
-    void handleExternalDragAndDropMotionNotify() {
-
+        return false;
     }
-    
-    void handleExternalDragAndDropButtonReleaseEvent() {
+    void handleExternalDragAndDropMotionNotify()
+    {
+    }
+
+    void handleExternalDragAndDropButtonReleaseEvent()
+    {
         cout << "DRAGGING FINISHED";
         dragging = false;
     }
 
 private:
-
     SEL draggingFinishedSelector;
     bool dragging = false;
 };
